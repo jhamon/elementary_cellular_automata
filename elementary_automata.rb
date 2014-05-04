ea61fb152915ba52609ff4d7d47aa21c54f62866
Jen Hamon <jhamon@gmail.com>
2014-05-04 01:01:57 -0700

    Add cylindrical boundary conditions; Avoid unnecessary string->array conversions.

--- elementary_automata.rb
+++ elementary_automata.rb
@@ -3,29 +3,38 @@ require_relative 'printers'
 
 class ElementaryAutomata
   def initialize(options={})
-    @state   = options[:seed] || ['0']*40+['1']+['0']*40
+    @state   = options[:seed] || '0'*50+'1'+'0'*50
     @rule    = options[:rule] || Rule.new(126)
     @printer = options[:printer] || TerminalPrinter.new
   end
 
-  def start(n=50)
+  def start(n=500)
     @printer.print(@state)
-    n.times { self.step }
+    n.times { step }
   end
 
-  def step
-    apply_rules
-    @state = @next_state
-    @printer.print(@state)
-  end
-
-  def apply_rules
-    @next_state = @state.dup
-    (1...@state.length-2).to_a.each do |i|
-      triple = @state[i-1, 3].join('')
-      @next_state[i] = @rule.apply(triple)
+  private
+    def step
+      apply_rules
+      @state = @next_state
+      @printer.print(@state)
     end
-  end
+
+    def apply_rules
+      @next_state = @state.dup
+      (1...@state.length-1).to_a.each do |i|
+        triple = @state[i-1, 3]
+        @next_state[i] = @rule.apply(triple)
+      end
+      apply_cylindrical_boundary_condition
+    end
+
+    def apply_cylindrical_boundary_condition
+      left_triple = @state[-1] + @state[0, 2]
+      @next_state[0] = @rule.apply(left_triple)
+      right_triple = @state[-2, 2] + @state[0]
+      @next_state[-1] = @rule.apply(right_triple)
+    end
 end
 
 auto = ElementaryAutomata.new
--- printers.rb
+++ printers.rb
@@ -2,6 +2,6 @@ class TerminalPrinter
   SYMBOLS = {'0' => ' ', '1' => '█'}
 
   def print(data_array)
-    puts data_array.map { |sym| SYMBOLS[sym] }.join('')
+    puts data_array.split('').map { |sym| SYMBOLS[sym] }.join('')
   end
 end
