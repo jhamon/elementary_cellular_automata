78327d8ddba38175d0eb178410067f514f062d21
Jen Hamon <jhamon@gmail.com>
2014-05-04 11:13:53 -0700

    Validate rule number.

--- rule.rb
+++ rule.rb
@@ -1,5 +1,11 @@
+class InvalidRuleError < StandardError
+  # Raised when trying to create a rule outside
+  # the valid range of (0..255).
+end
+
 class Rule
   def initialize(rule_number)
+    valid_rule?(rule_number)
     parse_rule_number(rule_number)
     build_rules_hash
   end
@@ -9,6 +15,10 @@ class Rule
   end
 
   private
+    def valid_rule?(rule_number)
+      raise InvalidRuleError if !(0..255).to_a.include?(rule_number)
+    end
+
     def parse_rule_number(rule_number)
       @settings = rule_number.to_s(2)
                              .rjust(8, '0')
